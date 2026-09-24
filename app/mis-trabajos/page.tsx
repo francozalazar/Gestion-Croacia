d753d8e40bb95c9a5a60a2791d1eb36d81b571e1
Franco Zalazar <88504313+francozalazar@users.noreply.github.com>
2026-09-24 12:31:56 -0300

    Redirect 'Mis Trabajos' to 'Trabajos' page
    Redirect users from the old 'Mis Trabajos' page to the new 'Trabajos' page.

--- app/mis-trabajos/page.tsx
+++ app/mis-trabajos/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
+// Pagina reemplazada por /trabajos. Redirigimos por si alguien la tenia guardada.
 export default function MisTrabajosPage() {
   redirect("/trabajos");
 }
